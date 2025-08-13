654c91758c4a548ad4ea9c7aaba497ca6852580c
nubskr <hello@nubskr.com>
2025-08-13 21:53:45 +0200

    update entry docs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use std::collections::HashMap;
 use std::fs::File;
+use std::hash::Hash;
 use std::io::{self, Seek, SeekFrom, Write};
+
+struct TableMetaStore {
+    data: HashMap<String,Vec<u64>>
+}
+
+impl TableMetaStore {
+    fn new() -> Self {
+        Self {
+            data: HashMap::new()
+        }
+    }
+
+    fn get_current_page(&self, column: &str) -> Option<u64> {
+        self.data.get(column)?.last().copied()
+
+    }
+}
 
 struct Entry {
     prefix_meta: String,
@@ -30,9 +49,35 @@ impl Page {
         }
     }
 
-    fn add_entry() {
-        // hmm, what now
+    fn add_entry(&mut self, entry: Entry) {
+        // what does it means to add an entry in a page
+        self.entries.push(entry);
+
+        // now its done in memory, we need to do it on disk as well
     }
+}
+
+
+// makes a new page and returns its offset
+fn make_new_page_at_EOF() -> io::Result<(u64)>{
+    // make a new page at EOF
+    
+    // return the offset
+    Ok((69))
+}
+
+fn append_to_column(tableMetaStore: TableMetaStore, column: &str, data: &str) -> io::Result<()> {
+    // find out the current page from table meta store
+    let pageOffset = tableMetaStore.get_current_page(column);
+    
+    // create entry
+
+
+    // check if that page has enough space to accomodate the entry
+
+    // if not, create a new page
+
+    Ok(())
 }
 
 fn do_shit_to_file(data: &[u8], fd: &mut File) -> io::Result<String> {
@@ -68,3 +113,33 @@ appends shit to the file it belongs to, that's it
 
 
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
