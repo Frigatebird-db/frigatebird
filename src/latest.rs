23d2670e7a00e4749bb309369e05b3acfedadfbd
Daksh Rinwan <dakshrinwan2003@gmail.com>
2025-08-18 00:04:23 +0200

    fuck off

---
+++ src/latest.rs
@@ -0,0 +1,14 @@
+// how the hell do I implement latest
+
+/*
+we NEED to store this contigiously and in-place modifications are ALLOWED
+(how to handle page splits ?)
+
+this is essentially just the latest state for each column and would be the first thing a query looks for when it comes
+
+we essentially need to have that 
+
+what the fuck man, cant really think
+
+fuck thsi shit man, living this life is suffocating 
+*/
