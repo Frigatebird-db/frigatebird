dafac7d624b9f9dd6661216b836150e50c775403
nubskr <hello@nubskr.com>
2025-08-26 02:26:32 +0200

    cleanup

--- src/ops_handler.rs
+++ src/ops_handler.rs
@@ -4,25 +4,14 @@ use crate::page_cache::CombinedCache;
 use crate::compressor::Compressor;
 
 fn upsert_data_into_column(entry: Entry, compressor: Compressor, meta_store: TableMetaStore, mut cache: CombinedCache, col: &str,data: &str) -> Result<bool, Box<dyn std::error::Error>> {
-    // get the latest page for this col from meta store
     let latest_page_meta = meta_store.get_latest_page_meta(col).unwrap();
     let page_id = latest_page_meta.id.clone();
-    // todo: make an entry out of this data string
     let entry = Entry::new(data);
 
     // check if page in uncompressed cache, if yes, just update and flush 
     if cache.uncompressed_pages.has(&latest_page_meta.id) {
-        // update and flush
-
-        // fetch the page
-
-        // update 
-
-        // umm, how to 'flush' exactly ??? well, that's what todos are for :skull:
-        // well, past nubskr did that for us, wise guy huh
-
-        // I think we nedd to update the page first lel, how do we do that ? what's a page btw ? 
         cache.uncompressed_pages.get(&page_id).unwrap().page.page.add_entry(entry);
+        // note tha the below stuff might totally own the cache and drain it once gone out of scope
         let ok = compressor.compress(meta_store,cache,&page_id).unwrap();
         return Ok((ok))
     }
