4005b77155108770331c57151d72b3ae39fef879
nubskr <hello@nubskr.com>
2025-11-10 22:48:58 +0100

    add sharded writer pool, better cross table throughput, still serialized single table mutations, nice stuff

--- src/writer/mod.rs
+++ src/writer/mod.rs
@@ -1,6 +1,11 @@
+use std::sync::atomic::AtomicUsize;
+
 pub mod allocator;
 pub mod executor;
 pub mod update_job;
+
+/// Controls how many writer shards are spawned by default.
+pub static GLOBAL_WRITER_SHARD_COUNT: AtomicUsize = AtomicUsize::new(1);
 
 pub use allocator::{DirectBlockAllocator, PageAllocator};
 pub use executor::{DirectoryMetadataClient, MetadataClient, Writer};
